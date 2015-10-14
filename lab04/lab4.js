8066198428ec6afe1b77b220fdbcb07f4a54a844
Chris Chappell <chappc@rpi.edu>
2015-10-14 00:34:41 -0400

    Added more functionality to lab4.js

--- lab04/lab4.js
+++ lab04/lab4.js
@@ -4,10 +4,41 @@
 // prevent javascript from loading before the document
 $("document").ready( function()
 {
+  function makeElement ( tag, className )
+  {
+    var element = document.createElement(tag);
+    element.className = className;
+    return element;
+  }
+
+  function addClass ( element, className )
+  {
+    if ( element.className.length > 0 )
+    {
+      element.className += " " + className;
+    }
+    else
+    {
+      element.className = className;
+    }
+    return element;
+  }
+
   function loadAlbum ()
   {
     function readJSON ( playlist )
     {
+      // create container list
+      var row_c = makeElement( "UL", "row-container" );
+      // create header
+      var row_h = makeElement( "LI", "row header" );
+      // create generic song
+      var gen_col_c = makeElement( "UL", "col-container" );
+      var gen_col = makeElement( "LI", "col" );
+      gen_col.appendChild( makeElement( "DIV", "container" ) );
+
+
+      row_c.appendChild(row_h);
       for ( var i = 0; i < playlist.length; ++i )
       {
         song = playlist[i];
